d8258a2f3a80c97cbf62ccb474ea5227aa684000
Pierr Daniel <pchinol11@gmail.com>
2026-09-09 23:02:41 -0300

    Add calculadora

---
+++ desafio01-calculadora/src/App.js
@@ -0,0 +1,55 @@
+import Input from './components/Input';
+import Button from './components/Button';
+
+import { Container, Content, Row } from './components/styles';
+import { useState } from 'react';
+
+function App() {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`);
+  }
+
+  const handleClear = () => {
+    setCurrentNumber('0');
+  }
+
+  const handleCalculate = () => {
+    setCurrentNumber(eval(currentNumber).toString());
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber} />
+        <Row>
+          <Button label="x" onClick={() => handleAddNumber('*')}/>
+          <Button label="/" onClick={() => handleAddNumber('/')}/>
+          <Button label="C" onClick={() => handleClear()}/>
+          <Button label="=" onClick={() => handleCalculate()}/>
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')}/>
+          <Button label="8" onClick={() => handleAddNumber('8')}/>
+          <Button label="9" onClick={() => handleAddNumber('9')}/>
+          <Button label="-" onClick={() => handleAddNumber('-')}/>
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')}/>
+          <Button label="5" onClick={() => handleAddNumber('5')}/>
+          <Button label="6" onClick={() => handleAddNumber('6')}/>
+          <Button label="+" onClick={() => handleAddNumber('+')}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="=" onClick={() => handleCalculate()}/>
+        </Row>
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
